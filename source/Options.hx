69cc6d257f1b38bad6a88b678aaed27d9c37dccc
Joaocoder2804 <106628166+Joaocoder2804@users.noreply.github.com>
2022-07-26 15:57:59 -0300

    Update Options.hx
    Controles sla

--- source/Options.hx
+++ source/Options.hx
@@ -4,3 +4,24 @@ class Options
 {
 	public static var masterVolume:Float = 1;
 }
+class AndroidControls extends Option
+{
+        public function new()
+        {
+                super();
+        }
+
+        public override function press():Bool
+        {
+                trace("switch");
+                FlxG.switchState(new android.AndroidControlsMenu());
+                return false;
+        }
+
+        private override function updateDisplay():String
+        {
+
+                return "Android Controls";
+         
+         }
+}
